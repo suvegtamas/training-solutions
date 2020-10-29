4b174e3cfea9e2a62d3af2a4129c5d1ae1b090a3
suvegtamas <suvegtamas97@gmail.com>
2020-10-29 22:36:13 +0100

    Bank

---
+++ src/main/java/classstructurecontructors/Bank.java
@@ -0,0 +1,55 @@
+package classstructurecontructors;
+
+import java.util.Scanner;
+
+public class Bank {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Account number?");
+        String accountNumber = scanner.nextLine();
+
+        System.out.println("Owner?");
+        String owner = scanner.nextLine();
+
+        System.out.println("New Balance?");
+        int amount = scanner.nextInt();
+        scanner.nextLine();
+
+        BankAccount account1 = new BankAccount(accountNumber, owner, amount);
+
+        System.out.println("New account number?");
+        String accountNumber2 = scanner.nextLine();
+
+        System.out.println("Owner?");
+        String owner2 = scanner.nextLine();
+
+        System.out.println("New Balance?");
+        int amount2 = scanner.nextInt();
+
+        BankAccount account2 = new BankAccount(accountNumber2, owner2, amount2);
+
+        System.out.println(account1.getInfo());
+        System.out.println(account2.getInfo());
+
+        System.out.println("Deposit amount?");
+        int depositAmount = scanner.nextInt();
+        account1.deposit(depositAmount);
+
+        System.out.println(account1.getInfo());
+
+        System.out.println("Withdraw amount?");
+        int withdrawAmount = scanner.nextInt();
+        account2.withdraw(withdrawAmount);
+
+        System.out.println(account2.getInfo());
+
+        System.out.println("Transfer amount?");
+        int transferAmount = scanner.nextInt();
+        account1.transfer(account2, transferAmount);
+
+        System.out.println(account1.getInfo());
+        System.out.println(account2.getInfo());
+    }
+
+}
---
+++ src/main/java/classstructurecontructors/BankAccount.java
@@ -0,0 +1,26 @@
+package classstructurecontructors;
+
+public class BankAccount {
+    private String accountNumber;
+    private String owner;
+    private int balance;
+
+    public BankAccount(String accountNumber, String owner, int balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.owner = owner;
+    }
+    public void deposit(int amount) {
+        balance = balance + amount;
+    }
+    public void withdraw(int amount) {
+        balance = balance - amount;
+    }
+    public void transfer(BankAccount to, int amount) {
+        withdraw(amount);
+        to.deposit(amount);
+    }
+    public String getInfo() {
+        return owner + " (" + accountNumber + "):" + balance + " Ft";
+    }
+}
