eddfbfa86ed8723be218dfd6cfed33d4abdec7a9
suvegtamas <suvegtamas97@gmail.com>
2020-11-11 20:20:53 +0100

    circle

---
+++ src/main/java/doublenumbers/CircleMain.java
@@ -0,0 +1,18 @@
+package doublenumbers;
+
+public class CircleMain {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(8);
+        Circle c2 = new Circle(12);
+        System.out.println(c1.perimeter());
+        System.out.println(c1.area());
+
+        System.out.println( "C2");
+
+        System.out.println(c2.perimeter());
+        System.out.println(c2.area());
+    }
+
+
+
+}
