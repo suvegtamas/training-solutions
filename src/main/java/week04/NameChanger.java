11f7aba6037d3f8d99245df572fb54368144611a
suvegtamas <suvegtamas97@gmail.com>
2020-11-16 13:48:55 +0100

    feladat week04

---
+++ src/main/java/week04/NameChanger.java
@@ -0,0 +1,31 @@
+package week04;
+
+public class NameChanger {
+    private String fullName;
+
+    public NameChanger(String fullName) {
+        if ( fullName == null || "".equals(fullName)) {
+            throw new IllegalArgumentException("Invalid Name:" + fullName);
+        }
+        this.fullName = fullName.toString();
+    }
+    public String changeFirstName(String firstName) {
+
+        String[] separateNames = fullName.split(" ");
+        String familyName = separateNames[0];
+        String oldFirstName = separateNames[1];
+        return fullName.replace(oldFirstName, firstName);
+
+
+
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    @Override
+    public String toString() {
+        return getFullName();
+    }
+}
---
+++ src/test/java/week04/NameChangerTest.java
@@ -0,0 +1,23 @@
+package week04;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class NameChangerTest {
+
+    @Test
+    void NameChangerTest() {
+
+        assertEquals("John Doe", new NameChanger("John Doe").getFullName());
+       // Exception ex = assertThrows(IllegalArgumentException.class, () -> new NameChanger(" "));
+       // assertEquals("Invalid name:", ex.getMessage());
+    }
+    @Test
+    void changeNameTest() {
+        NameChanger name = new NameChanger("John Doe");
+        assertEquals("John Vans", name.changeFirstName("Vans"));
+
+    }
+}
