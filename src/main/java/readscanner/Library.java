b6731027a83b4a32b112e91a7869dde12dc837aa
suvegtamas <suvegtamas97@gmail.com>
2020-11-19 11:36:55 +0100

    library

---
+++ src/main/java/readscanner/Library.java
@@ -0,0 +1,29 @@
+package readscanner;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Library {
+    private List<Book> booksList = new ArrayList<>();
+
+    public void loadFromFile() {
+        try(Scanner sc = new Scanner(Library.class.getResourceAsStream("/library.txt"))) {
+            sc.useDelimiter(";|(\r\n)|\n");
+            while(sc.hasNextLine()) {
+                String regNum = sc.next();
+                String author = sc.next();
+                String title = sc.next();
+                int yearOfPublish = sc.nextInt();
+                booksList.add(new Book(regNum, author, title, yearOfPublish));
+            }
+        }
+
+
+
+    }
+    public List<Book> getBooks() {
+        return booksList;
+    }
+}
