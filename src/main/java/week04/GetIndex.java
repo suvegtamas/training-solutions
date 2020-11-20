30425107fc272c7e28c244afe0031872aeeb6277
suvegtamas <suvegtamas97@gmail.com>
2020-11-20 13:24:53 +0100

    getindex

---
+++ src/main/java/week04/GetIndex.java
@@ -0,0 +1,23 @@
+package week04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetIndex {
+    public static void main(String[] args) {
+        GetIndex g = new GetIndex();
+
+        System.out.println(g.getIndexesOfChar("test",'s'));
+
+    }
+    public List<Integer> getIndexesOfChar(String str, char c) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == c) {
+                result.add(i);
+            }
+        }
+        return result;
+
+    }
+}
