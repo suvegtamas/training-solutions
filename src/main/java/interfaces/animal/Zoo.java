62da29f58fefe7d0804c7833d4999f09f6035c2b
suvegtamas <suvegtamas97@gmail.com>
2020-12-13 23:09:25 +0100

    Animal feladat + Test

---
+++ src/main/java/interfaces/animal/Zoo.java
@@ -0,0 +1,24 @@
+package interfaces.animal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zoo {
+    private List<Animal> animals = new ArrayList<>();
+
+    public Zoo(List<Animal> animals) {
+        this.animals = animals;
+    }
+    public int getNumberOfAnimals() {
+        return animals.size();
+    }
+    public int getNumberOfLegs() {
+        int legs = 0;
+        for (Animal animal : animals) {
+            legs += animal.getNumberOfLegs();
+        }
+        return legs;
+    }
+}
+
+
