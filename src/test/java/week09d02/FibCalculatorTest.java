d7ba1ced90333f23210f32238bd4a7a75cc079fe
suvegtamas <suvegtamas97@gmail.com>
2020-12-22 21:35:08 +0100

    FibCalculator + Test

---
+++ src/test/java/week09d02/FibCalculatorTest.java
@@ -0,0 +1,18 @@
+package week09d02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FibCalculatorTest {
+    @Test
+    void testFibSum() {
+        FibCalculator f = new FibCalculator();
+        assertEquals(2,f.sumEvens(5));
+        assertEquals(44,f.sumEvens(10));
+
+    }
+
+
+
+}
