e872bf82871c44829a54bf5e36244b74841cd723
suvegtamas <suvegtamas97@gmail.com>
2021-01-26 20:52:19 +0100

    Talentshow feladat

---
+++ src/main/java/ioprintwriter/Vote.java
@@ -0,0 +1,26 @@
+package ioprintwriter;
+
+public class Vote {
+
+    private long id;
+
+    private int number;
+
+    public Vote(long id, int num) {
+        this.id = id;
+        this.number = num;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void incNum() {
+        this.number += 1;
+    }
+}
+
