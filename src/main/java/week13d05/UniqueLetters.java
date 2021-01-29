6386a551f41ae72306030e1aea242be61c216282
suvegtamas <suvegtamas97@gmail.com>
2021-01-29 13:35:08 +0100

    Napi  feladat.

--- src/main/java/week13d05/UniqueLetters.java
+++ src/main/java/week13d05/UniqueLetters.java
@@ -6,13 +6,21 @@ import java.util.Set;
 
 public class UniqueLetters {
     public int uniqueLetterCounter(String word) {
-        Set<Character> result = new HashSet<>();
+      Set<Character> result = new HashSet<>();
         for(Character c : word.toLowerCase().toCharArray()) {
-            result.add(c);
+            if(checkLetter(c)) {
+                result.add(c);
+            }
         }
         return result.size();
     }
-   
+    public boolean checkLetter(char c) {
+        if(c > 'a' && c < 'z') {
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         UniqueLetters ul = new UniqueLetters();
         int result = ul.uniqueLetterCounter("aabbc");
